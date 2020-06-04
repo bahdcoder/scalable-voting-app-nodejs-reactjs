dbabe3bb006aff430bd1e204811728ee66e0309c
Frantz Kati <bahdcoder@gmail.com>
2020-06-04 05:03:45 +0100

    17-casting-votes

--- frontend/src/pages/ViewPoll.js
+++ frontend/src/pages/ViewPoll.js
@@ -3,7 +3,9 @@ import React, { useEffect, useState } from 'react'
 import Button from '../components/Button'
 
 export default function ViewPoll({ match }) {
+    const [ip, setIp] = useState(null)
     const [poll, setPoll] = useState(null)
+    const [voted, setVoted] = useState(false)
 
     const fetchPoll = async () => {
         const response = await fetch(`${process.env.REACT_APP_API_URL}/polls/${match.params.poll}`)
@@ -13,9 +15,36 @@ export default function ViewPoll({ match }) {
         setPoll(data)
     }
 
+    const fetchClientIpAddress = async () => {
+        const response = await fetch('https://ipapi.co/json')
+
+        const data = await response.json()
+
+        setIp(data.ip)
+    }
+
     useEffect(() => {
         fetchPoll()
     }, [])
+
+    useEffect(() => {
+        fetchClientIpAddress()
+    }, [])
+
+    const vote = async (choice) => {
+        await fetch(`${process.env.REACT_APP_API_URL}/polls/${match.params.poll}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                ip,
+                choice
+            })
+        })
+
+        setVoted(true)
+    }
 
     return (
         <div className="container mx-auto mt-16 px-5">
@@ -34,7 +63,9 @@ export default function ViewPoll({ match }) {
                             <div className='px-5 py-4 border-t border-gray-400 flex justify-between items-center' key={choice._id}>
                                 {choice.name}
 
-                                <Button>Vote</Button>
+                                {!voted ? (
+                                    <Button onClick={() => vote(choice._id)}>Vote</Button>
+                                ): <span className='text-blue-500'>29</span>}
                             </div>
                         )
                     })}
